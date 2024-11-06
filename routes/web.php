a015bddd22b1e6203501e215961acfbddaeddeb9
iftykhar alam <s.m.ifty49@gmail.com>
2024-11-06 16:50:34 +0600

    Action Controller and view setting up

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ActionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/Action',ActionController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
