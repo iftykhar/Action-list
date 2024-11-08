fbd432ad0330968f54ea4c1dd9f8c35d2f45722b
iftykhar alam <s.m.ifty49@gmail.com>
2024-11-08 23:45:28 +0600

    changering seeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +19,16 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::factory()->create([
+            "name"=> "iftykhar alam",
+            "email"=> "iftykhar@example.com",
+
+        ]);
+
+        DB::table("users")->insert([
+            'user_id'=>DB::table('users'),
+
+        ]);
     }
 }
