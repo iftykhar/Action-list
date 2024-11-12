81eb0b83f347752ba922dc80eb6241243e8ed58b
iftykhar alam <s.m.ifty49@gmail.com>
2024-11-12 23:49:22 +0600

    minor change in view file

--- resources/views/Action/create.blade.php
+++ resources/views/Action/create.blade.php
@@ -11,7 +11,7 @@
                 <div class="p-6 text-gray-900">
                     {{-- {{ __("You're logged in!") }} --}}
 
-                    <form class="max-w-sm mx-auto " method="POST">
+                    <form class="max-w-sm mx-auto " action="{{ Action.store }}" method="POST">
                         @csrf
                         <label for="content"  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Action Submtion</label>
                         <textarea id="content" name="content" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2" placeholder="Leave a comment..." maxlength="255"></textarea>
